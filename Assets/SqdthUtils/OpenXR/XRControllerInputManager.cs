d9533c9549010c9bf062b5ae7ccde367735690b4
Keith Lerner <46833242+KeithLerner@users.noreply.github.com>
2024-08-28 11:09:56 -0400

    More updates to xr controller input manager
    still work in progress

--- Assets/SqdthUtils/OpenXR/XRControllerInputManager.cs
+++ Assets/SqdthUtils/OpenXR/XRControllerInputManager.cs
@@ -1,23 +1,30 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using XR = UnityEngine.XR;
+using InputDevice = UnityEngine.XR.InputDevice;
 
 namespace SqdthUtils.OpenXR
 {
     public class XRControllerInputManager : MonoBehaviour
     {
         public static XRControllerInputManager Singleton;
+
+        public InputDevice LeftInputDevice { get; private set; }
+        public InputDevice RightInputDevice { get; private set; }
         
-        public ActionBasedController left;
-        public ActionBasedController right;
+        public ActionBasedController LeftController { get; private set; }
+        public ActionBasedController RightController { get; private set; }
 
         public UnityEvent<InputAction.CallbackContext> onLeftActivateAction;
         public UnityEvent<InputAction.CallbackContext> onRightActivateAction;
 
         private void Awake()
         {
+            // Set up singleton instance
             if (Singleton == null)
                 Singleton = this;
             else
@@ -26,9 +33,34 @@ namespace SqdthUtils.OpenXR
                     "Multiple instances of XRControllerInputManager found, " +
                     "destroying duplicate instances.");
                 Destroy(gameObject);
+                return;
             }
             
-            Debug.Log(left.);
+            // TESTING
+            var inputDevices = new List<InputDevice>();
+            XR.InputDevices.GetDevices(inputDevices);
+            foreach (var device in inputDevices)
+            {
+                Debug.Log($"Device found with name '{device.name}'.");
+            }
+        }
+
+        private void Update()
+        {
+            // TESTING
+            string debug = "";
+            float grip;
+            if (LeftInputDevice.TryGetFeatureValue(XR.CommonUsages.grip,
+                    out grip))
+            {
+                debug += $"Left: {grip}\n";
+            }
+            if (RightInputDevice.TryGetFeatureValue(XR.CommonUsages.grip,
+                    out grip))
+            {
+                debug += $"Right: {grip}";
+            }
+            Debug.Log(debug);
         }
 
         public void DebugCallbackContext(InputAction.CallbackContext context)
@@ -48,14 +80,52 @@ namespace SqdthUtils.OpenXR
 
         private void OnEnable()
         {
-            left.activateAction.action.performed += OnLeftActivateAction;
-            right.activateAction.action.performed += OnRightActivateAction;
+            // Get XR input devices
+            LeftInputDevice = XR.InputDevices.GetDeviceAtXRNode(XR.XRNode.LeftHand);
+            RightInputDevice = XR.InputDevices.GetDeviceAtXRNode(XR.XRNode.RightHand);
+
+            // Get action based controllers
+            ActionBasedController[] controllers =
+                GetComponentsInChildren<ActionBasedController>();
+            foreach (ActionBasedController abc in controllers)
+            {
+                if (abc.gameObject.name.ToLower().Contains("left"))
+                {
+                    LeftController = abc;
+                }
+                else if (abc.gameObject.name.ToLower().Contains("right"))
+                {
+                    RightController = abc;
+                }
+            }
+            
+            // Set up action based controller events if controllers were found
+            if (LeftController == null)
+            {
+                Debug.LogError("Failed to find Left Action Based Controller.");
+            }
+            else
+            {
+                LeftController.activateAction.action.performed +=
+                    OnLeftActivateAction;
+            }
+            if (RightController == null)
+            {
+                Debug.LogError("Failed to find Right Action Based Controller.");
+            }
+            else
+            {
+                RightController.activateAction.action.performed +=
+                    OnRightActivateAction;
+            }
         }
 
         private void OnDisable()
         {
-            left.activateAction.action.performed -= OnLeftActivateAction;
-            right.activateAction.action.performed -= OnRightActivateAction;
+            LeftController.activateAction.action.performed -= 
+                OnLeftActivateAction;
+            RightController.activateAction.action.performed -= 
+                OnRightActivateAction;
         }
     }
 }
